cf73da9358bc56d56dde63b01a5e356a4d29e004
Ryan Helferich <ryan.helferich@snhu.edu>
2021-11-11 22:07:55 -0600

    added filesync method to travel controller to read json data (temporarily)

--- app_server/controllers/travel_controller.js
+++ app_server/controllers/travel_controller.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips });
 };
 
 module.exports = {
