7c662d7d379f110c9d4df5934c5267365d95439b
Ryan Helferich <ryan.helferich@snhu.edu>
2021-11-15 22:33:36 -0600

    Travel images now have http end points. Travel controller makes HTTP requests instead of static JSON

--- app_server/controllers/travel_controller.js
+++ app_server/controllers/travel_controller.js
@@ -1,5 +1,11 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+//REPLACE WITH HTTPS REQUEST MODULE INSTEAD OF STATIC JSON
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+};
 
 /* GET travel view */
 const travel = (req, res) => {
