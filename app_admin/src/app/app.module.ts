d47e5142c859df87e7eb881eb251c9a8541b4a56
Ryan Helferich <ryan.helferich@snhu.edu>
2021-11-30 23:10:59 -0600

    Created seperate component for trip-cards

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { TripCardComponent } from './trip-card/trip-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TripListingComponent
+    TripListingComponent,
+    TripCardComponent
   ],
   imports: [
     BrowserModule
---
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-card',
+  templateUrl: './trip-card.component.html',
+  styleUrls: ['./trip-card.component.css']
+})
+export class TripCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
