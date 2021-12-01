999252041afe2d0173545322568d1cfc8e1e7384
Ryan Helferich <ryan.helferich@snhu.edu>
2021-11-30 23:44:27 -0600

    Added addTrip to router

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TripListingComponent } from './trip-listing/trip-listing.component';
 import { TripCardComponent } from './trip-card/trip-card.component';
-
+import { TripDataService } from 'src/app/services/trip-data.service';
+import { AddTripComponent } from './add-trip/add-trip.component';
 @NgModule({
   declarations: [
     AppComponent,
     TripListingComponent,
-    TripCardComponent
+    TripCardComponent,
+    AddTripComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    TripDataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
