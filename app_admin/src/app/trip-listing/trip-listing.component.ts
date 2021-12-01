8be55b75c07413fbb660b368e4fb5ca0efe78f14
Ryan Helferich <ryan.helferich@snhu.edu>
2021-11-30 23:43:18 -0600

    Added addTrip through router

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,18 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { trips } from '../data/trips';
+import { Router } from "@angular/router";
+// import { trips } from '../data/trips';
+import { TripDataService } from '../services/trip-data.service';
+import { Trip } from '../models/trip';
 
 @Component({
   selector: 'app-trip-listing',
   templateUrl: './trip-listing.component.html',
-  styleUrls: ['./trip-listing.component.css']
+  styleUrls: ['./trip-listing.component.css'],
+  providers: [TripDataService]
 })
+
 export class TripListingComponent implements OnInit {
 
-  trips: Array<any> = trips;
+  // trips: Array<any> = trips;
 
-  constructor() { }
+  trips: Trip[];
+  
+  message: string;
 
-  ngOnInit(): void {
+  constructor(
+    private tripDataService: TripDataService,
+    private router: Router
+    ) { }
+    
+  private addTrip(): void {
+    this.router.navigate(['add-trip']);
   }
 
+  private getTrips(): void {
+    console.log('Inside TripListingComponent#getTrips');
+    this.message = 'Searching for trips';
+    this.tripDataService
+    .getTrips()
+      .then(foundTrips => {
+        this.message = foundTrips.length > 0 ? '' : 'No trips found';
+        this.trips = foundTrips;
+      });
+  }
+
+ 
+
+ngOnInit(): void {
+  this.getTrips();
+  }
 }
