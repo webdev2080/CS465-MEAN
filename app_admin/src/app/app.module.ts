289e928590301b61b74e67db5059bdda9784365a
Ryan Helferich <ryan.helferich@snhu.edu>
2021-11-30 22:45:36 -0600

    Imported trips, converted html to use selectors from output

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TripListingComponent
   ],
   imports: [
     BrowserModule
---
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { trips } from '../data/trips';
+
+@Component({
+  selector: 'app-trip-listing',
+  templateUrl: './trip-listing.component.html',
+  styleUrls: ['./trip-listing.component.css']
+})
+export class TripListingComponent implements OnInit {
+
+  trips: Array<any> = trips;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
