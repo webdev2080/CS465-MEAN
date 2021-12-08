3f85b8f8a27631896150a3079f93b8b1e3c15c2b
Ryan Helferich <ryan.helferich@snhu.edu>
2021-12-07 23:08:17 -0600

    Added login,register and auth endpoints

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -2,11 +2,17 @@ import { Injectable , Inject } from '@angular/core';
 import { Http } from '@angular/http';
 
 import { Trip } from '../models/trip';
+import { BROWSER_STORAGE } from '../storage';
+import { User } from '../models/user';
+import { AuthResponse } from '../models/authresponse';
 
 @Injectable()
 export class TripDataService {
 
-  constructor(private http: Http) { }
+  constructor(
+    private http: Http,
+    @Inject(BROWSER_STORAGE) private storage: Storage
+    ) { }
 
   private apiBaseUrl = 'http://localhost:3000/api/';
   private tripUrl = `${this.apiBaseUrl}trips/`;
@@ -20,19 +26,19 @@ export class TripDataService {
       .catch(this.handleError);
   }
 
-  public getTrip(tripCode: string): Promise<Trip[]> {
-    console.log('Inside TripDataService#getTrips');
+  public getTrip(tripCode: string): Promise<Trip> {
+    console.log('Inside TripDataService#getdTrip');
     return this.http
       .get(this.tripUrl + tripCode)
       .toPromise()
-      .then(response => response.json() as Trip[])
+      .then(response => response.json() as Trip)
       .catch(this.handleError);
   }
 
   public addTrip(formData: Trip): Promise<Trip> {
     console.log('Inside TripDataService#addTrip');
     return this.http
-      .post(`${this.apiBaseUrl}trips`, formData)
+      .post(this.tripUrl, formData)
       .toPromise()
       .then(response => response.json() as Trip[])
       .catch(this.handleError);
@@ -50,5 +56,22 @@ export class TripDataService {
   private handleError(error: any): Promise<any> {
     console.error('Something has gone wrong', error); // for demo purposes only
     return Promise.reject(error.message || error);
+  }
+
+  public login(user: User): Promise<AuthResponse> {
+     return this.makeAuthApiCall('login', user);
+  }
+
+  public register(user: User): Promise<AuthResponse> {
+     return this.makeAuthApiCall('register', user);
+  }
+
+  private makeAuthApiCall(urlPath: string, user: User): Promise<AuthResponse> {
+    const url: string = `${this.apiBaseUrl}/${urlPath}`;
+    return this.http
+      .post(url, user)
+      .toPromise()
+      .then(response => response.json() as AuthResponse)
+      .catch(this.handleError);
   }
 }
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -7,6 +7,7 @@ const jwt = require('express-jwt');
 
 const auth = jwt({
     secret: process.env.JWT_SECRET,
+    algorithms: ['HS256'],
     userProperty: 'payload'
 });
 
